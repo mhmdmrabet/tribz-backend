9c2fed281b5138fd8d66d8908d498e9dc5af6f6b
Mohamed M'rabet <mhmdmrabet@yahoo.fr>
2021-05-06 13:04:12 +0200

    Mise en place du Users Controller

--- start/routes/users.ts
+++ start/routes/users.ts
@@ -1,9 +1,10 @@
 import Route from '@ioc:Adonis/Core/Route';
+import UsersController from 'App/Controllers/Http/UsersController';
 
 Route.group(() => {
   Route.group(() => {
-    Route.get('', async () => {
-      return { message: 'Users Route' };
+    Route.get('', async (ctx) => {
+      return new UsersController().findAll(ctx);
     });
   }).prefix('/users');
 }).prefix('/api');
