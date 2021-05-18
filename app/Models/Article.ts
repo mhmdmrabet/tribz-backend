d729730e582add0b3ac723500e79c05b650d7fe8
Mohamed M'rabet <mhmdmrabet@yahoo.fr>
2021-05-18 11:01:29 +0200

    Ajout Model Article

---
+++ app/Models/Article.ts
@@ -0,0 +1,54 @@
+import { DateTime } from 'luxon';
+import { BaseModel, belongsTo, column } from '@ioc:Adonis/Lucid/Orm';
+import Brand from './Brand';
+import { BelongsTo } from '@ioc:Adonis/Lucid/Relations';
+
+export default class Article extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number;
+
+  @column()
+  public title: string;
+
+  @column()
+  public pictureHeader: string;
+
+  @column()
+  public stock: number;
+
+  @column()
+  public size: string;
+
+  @column()
+  public mention: string;
+
+  @column()
+  public overview: string;
+
+  @column()
+  public websiteLink: string;
+
+  @column()
+  public price: number;
+
+  @column()
+  public sizeMeasure: string;
+
+  @column()
+  public weightMeasure: string;
+
+  @column()
+  public dateLimit: DateTime;
+
+  @column()
+  public articleCompleted: boolean;
+
+  @column()
+  public brandId: number;
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime;
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime;
+}
