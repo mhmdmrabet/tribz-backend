c72d2deb59b25fb23642e1d14934068638e9c8b6
Mohamed M'rabet <mhmdmrabet@yahoo.fr>
2021-05-21 14:41:53 +0200

    Ajout de factory dans notre db

--- database/factories/index.ts
+++ database/factories/index.ts
@@ -1,11 +1,42 @@
 import Factory from '@ioc:Adonis/Lucid/Factory';
+import Article from 'App/Models/Article';
 import Brand from 'App/Models/Brand';
+import Interest from 'App/Models/Interest';
+import User from 'App/Models/User';
 
-export const BrandFactory = Factory.define(Brand, ({ faker }) => {
+export const UserFactory = Factory.define(User, () => {
+  return {
+    email: 'mohamed@tanke.fr',
+    password: 'mohamedpwd',
+    role: 'admin',
+    firstName: 'Mohamed',
+    country: 'France',
+  };
+}).build();
+
+export const ArticleFactory = Factory.define(Article, () => {
+  return {
+    title: 'Robe',
+    stock: 12,
+    price: 39.99,
+  };
+})
+  .relation('brand', () => BrandFactory)
+  .build();
+
+export const BrandFactory = Factory.define(Brand, () => {
   return {
     title: 'Zara',
     subtitle: 'prêt-à-porter',
-    description: faker.lorem.paragraphs(2),
-    logo: faker.lorem.word(1),
+    description: 'Marque internationnal du groupe Inditex',
+    logo: 'zara',
+  };
+})
+  .relation('articles', () => ArticleFactory)
+  .build();
+
+export const InterestFactory = Factory.define(Interest, () => {
+  return {
+    name: 'Mode',
   };
 }).build();
