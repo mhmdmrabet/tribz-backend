0b95f3f1539da6c4d08faa02ad3bb4780a110107
Mohamed M'rabet <mhmdmrabet@yahoo.fr>
2021-05-24 13:59:07 +0200

    CRUD + Validator table Brands

---
+++ app/Validators/CreateBrandValidator.ts
@@ -0,0 +1,18 @@
+import { schema, rules } from '@ioc:Adonis/Core/Validator';
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
+
+export default class CreateBrandValidator {
+  constructor(protected ctx: HttpContextContract) {}
+
+  public schema = schema.create({
+    title: schema.string({}, [
+      rules.unique({ table: 'brands', column: 'title' }),
+      rules.alpha({ allow: ['space'] }),
+    ]),
+    subtitle: schema.string.optional(),
+    description: schema.string.optional(),
+    logo: schema.string.optional({ trim: true }),
+  });
+
+  public messages = {};
+}
