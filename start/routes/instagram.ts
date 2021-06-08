3bb660600519e1d4772955e26eaadc309e8827c0
Mohamed <mhmdmrabet@yahoo.fr>
2021-06-08 12:45:02 +0200

    Add slashes for redirect uri

--- start/routes/instagram.ts
+++ start/routes/instagram.ts
@@ -1,14 +1,18 @@
 import Route from '@ioc:Adonis/Core/Route';
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
+import axios from 'axios';
 
 // == https://api.instagram.com/oauth/authorize?client_id=****&redirect_uri=****&scope=user_profile,user_media&response_type=code
 Route.group(() => {
-  Route.get('auth', async ({ request }: HttpContextContract) => {
+  Route.get('auth/', async ({ request }: HttpContextContract) => {
     const { code } = request.qs();
     const clientId = '2667165773583775';
     const clientSecret = 'b4e1ee3c2d415c3b5c66956863883541';
     const redirectUri = 'https://tribz-pg.herokuapp.com/api/auth';
-    const urlExchangeTheCodeForAToken = `  https://api.instagram.com/oauth/access_token/client_id=${clientId}&client_secret=${clientSecret}&grant_type=authorization_code&redirect_uri=${redirectUri}&code=${code}`;
+    const urlExchangeTheCodeForAToken = `https://api.instagram.com/oauth/access_token/client_id=${clientId}&client_secret=${clientSecret}&grant_type=authorization_code&redirect_uri=${redirectUri}&code=${code}`;
+
+    // const response = await axios.post(urlExchangeTheCodeForAToken);
+
     return { urlExchangeTheCodeForAToken };
   });
 }).prefix('api');
