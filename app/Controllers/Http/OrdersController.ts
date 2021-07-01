bbd4f89f2fd12699c413e79cc30a3e0a84e0b8ed
Mohamed <mhmdmrabet@yahoo.fr>
2021-07-01 15:11:45 +0200

    Refacto route Orders

--- app/Controllers/Http/OrdersController.ts
+++ app/Controllers/Http/OrdersController.ts
@@ -9,8 +9,9 @@ export default class OrdersController {
    * Récupère les orders d'un user
    * @returns []
    */
-  public async userOrders() {
-    const user = await User.findOrFail('e0a96603-71c3-4e50-9f25-d9e2e124cb74');
+  public async userOrders({ auth, response }: HttpContextContract) {
+    const id = await auth.user?.id;
+    const user = await User.findOrFail(id);
     await user.load('articles');
 
     const articles = user.articles;
@@ -25,9 +26,9 @@ export default class OrdersController {
 
       // ==> Récupération uniquement des info qui nous intérésse
       const article = {
-        id: fullArticle?.id,
-        title: fullArticle?.title,
-        price: fullArticle?.price,
+        id: fullArticle!.id,
+        title: fullArticle!.title,
+        price: fullArticle!.price,
       };
 
       // ==> Renvoi l'order formatter
@@ -39,13 +40,23 @@ export default class OrdersController {
       };
     });
 
-    return orders;
+    // == Si le user n'a pas d'order
+    if (!orders.length) {
+      return response.ok({
+        data: orders,
+        message: 'This user has no pending order',
+        success: true,
+      });
+    }
+
+    return { data: orders, success: true };
   }
 
-  public async store({ response, request }: HttpContextContract) {
+  public async store({ response, request, auth }: HttpContextContract) {
     // === https://docs.adonisjs.com/reference/orm/relations/many-to-many#attach
     try {
-      const user = await User.findOrFail('e0a96603-71c3-4e50-9f25-d9e2e124cb74');
+      const id = await auth.user!.id;
+      const user = await User.findOrFail(id);
       const { motivation, status, articleId } = request.body();
       const article = await Article.findOrFail(articleId);
       await user.related('articles').attach({
@@ -55,41 +66,67 @@ export default class OrdersController {
         },
       });
 
-      response.created();
+      return response.created({ message: 'Orders has been registred', success: true });
     } catch (error) {
       let message = error;
       if (error.code === '23505') {
         message = 'Unique violation';
       }
-      response.badRequest({ message });
+      return response.badRequest({ message, success: false });
     }
   }
 
   // ==> Detruit tout les rows de cette order ,
   // Ça ne doit pas poser problème car un un user n'a qu'un seul article
-  public async destroy({ params }: HttpContextContract) {
-    const user = await User.findOrFail('9126caff-2390-4b50-9086-f3bca3e9376a');
-    await user.related('articles').query();
+  // https://docs.adonisjs.com/reference/orm/relations/many-to-many#detach
+  public async destroy({ params, auth }: HttpContextContract) {
+    const userId = await auth.user?.id;
+    const user = await User.findOrFail(userId);
+    // == Remove the relationship from the pivot table
     await user.related('articles').detach([params.articleId]);
 
-    return { message: 'Go to Data Base' };
+    return { message: 'The order has been deleted', success: true };
   }
 
-  public async update({ request, response }: HttpContextContract) {
+  public async update({ request, response, params }: HttpContextContract) {
     try {
       const { post, motivation, status } = request.body();
-      const rows = await Database.from('article_user').where('id', 1).update(
-        {
-          status,
-          motivation,
-          post,
-          updated_at: DateTime.now().toString(),
-        },
-        ['status', 'motivation', 'post', 'updated_at']
-      );
-      response.ok({ order: rows[0] });
+      const rows: Promise<any> = await Database.from('article_user')
+        .where('id', params.id)
+        .update(
+          {
+            status,
+            motivation,
+            post,
+            updated_at: DateTime.now().toSQL(),
+          },
+          // Column to returns
+          ['status', 'motivation', 'post', 'updated_at']
+        )
+        .first();
+      return response.ok({ order: rows, success: true });
     } catch (error) {
-      response.badRequest({ error });
+      return response.badRequest({ message: error, success: false });
+    }
+  }
+
+  public async index() {
+    try {
+      const rows = await Database.from('article_user')
+        .join('users', 'users.id', '=', 'article_user.user_id')
+        .join('articles', 'articles.id', '=', 'article_user.article_id')
+        .select('article_user.id as orderId')
+        .select('users.first_name as firstName')
+        .select('users.last_name as lastName')
+        .select('articles.title')
+        .select('articles.price')
+        .select('article_user.status')
+        .select('article_user.motivation')
+        .select('article_user.post');
+
+      return { orders: rows, success: true };
+    } catch (error) {
+      return { message: error, success: false };
     }
   }
 }
--- start/routes/orders.ts
+++ start/routes/orders.ts
@@ -1,8 +1,12 @@
 import Route from '@ioc:Adonis/Core/Route';
 
 Route.group(() => {
+  Route.resource('orders', 'OrdersController').only(['update', 'store']);
   Route.get('orders', 'OrdersController.userOrders');
-  Route.post('orders', 'OrdersController.store');
-  Route.put('orders', 'OrdersController.update');
+  // === PERMET DE RECUPERER TOUTES LES COMMANDES
+  // ACCESIBLE SEULEMENT A L'ADMIN
+  Route.get('admin/orders', 'OrdersController.index').middleware('authAdmin');
   Route.delete('orders/:articleId', 'OrdersController.destroy');
-}).prefix('api');
+})
+  .prefix('api')
+  .middleware('auth');
