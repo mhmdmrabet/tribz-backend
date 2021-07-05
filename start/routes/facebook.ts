370814cb503b8724d50bbb3380536d8c4da99960
Mohamed <mhmdmrabet@yahoo.fr>
2021-07-05 12:08:12 +0200

    Add response type for API Graph

--- start/routes/facebook.ts
+++ start/routes/facebook.ts
@@ -13,7 +13,7 @@ Route.get('facebook/login', async ({ response }: HttpContextContract) => {
   response
     .redirect()
     .toPath(
-      `https://www.facebook.com/v11.0/dialog/oauth?client_id=${APP_ID}&display=popup&redirect_uri=${REDIRECT_URI}&state=26`
+      `https://www.facebook.com/v11.0/dialog/oauth?client_id=${APP_ID}&display=popup&response_type=tokenredirect_uri=${REDIRECT_URI}&state=26`
     );
 }).prefix('api');
 
@@ -26,21 +26,22 @@ Route.get('instagram', async ({ request, auth }: HttpContextContract) => {
       `https://graph.facebook.com/v11.0/oauth/access_token?client_id=${APP_ID}&redirect_uri=${REDIRECT_URI}&client_secret=${CLIENT_SECRET}&code=${code}`
     );
 
-    const { access_token: token, expires_in: expiresIn } = response.data;
+    // const { access_token: token, expires_in: expiresIn } = response.data;
 
-    const userId: string = await String(auth.user?.id);
-    const socialNetwork = 'facebook';
-    // ==> Create or Update Token in database
-    await Token.updateOrCreate(
-      { userId, socialNetwork },
-      {
-        token,
-        expiresIn,
-        socialNetwork,
-        userId,
-      }
-    );
-    return { message: "The user's token has been registered", success: true };
+    // const userId: string = await String(auth.user?.id);
+    // const socialNetwork = 'facebook';
+    // // ==> Create or Update Token in database
+    // await Token.updateOrCreate(
+    //   { userId, socialNetwork },
+    //   {
+    //     token,
+    //     expiresIn,
+    //     socialNetwork,
+    //     userId,
+    //   }
+    // );
+    // return { message: "The user's token has been registered", success: true };
+    return { result: response.data };
   } catch (error) {
     return { message: error, success: false };
   }
